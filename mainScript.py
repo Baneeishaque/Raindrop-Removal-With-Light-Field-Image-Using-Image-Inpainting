9fb4530aad5b748a8e36d9b6de1fb6df57d653c9
Banee Ishaque K <baneeishaque@gmail.com>
2021-04-14 12:17:31 +0530

    Jupyter notebook

--- mainScript.py
+++ mainScript.py
@@ -8,7 +8,7 @@ depthMapImage = cv.imread('depth_01.png', cv.IMREAD_ANYDEPTH)
 # converting to its binary form
 ret, binaryDepthMapImage = cv.threshold(depthMapImage, 127, 255, cv.THRESH_BINARY)
 
-cv.imshow("Original Depth Map Iamge", depthMapImage)
+cv.imshow("Original Depth Map Image", depthMapImage)
 cv.imshow("Binary Image of Depth Map Image", binaryDepthMapImage)
 
 kernel = np.ones((5, 5), np.uint8)
@@ -18,13 +18,14 @@ cv.imshow("Binary Image of Depth Map after Closing", binaryDepthMapImageWithClos
 binaryDepthMapImageWithClosingAndOpening = cv.morphologyEx(binaryDepthMapImageWithClosing, cv.MORPH_OPEN, kernel)
 cv.imshow("Binary Image of Depth Map after Closing & Opening", binaryDepthMapImageWithClosingAndOpening)
 
-binaryDepthMapImageWithClosingOpeningAndErosion = cv.erode(binaryDepthMapImageWithClosingAndOpening, kernel, iterations=1)
+binaryDepthMapImageWithClosingOpeningAndErosion = cv.erode(binaryDepthMapImageWithClosingAndOpening, kernel,
+                                                           iterations=1)
 cv.imshow("Binary Image of Depth Map after Closing, Opening & Erosion", binaryDepthMapImageWithClosingOpeningAndErosion)
 
 lightFieldImage = cv.imread('img_01.png')
 
 highPassFilter = np.array([[0, -1, 0],
-                           [-1,  5, -1],
+                           [-1, 5, -1],
                            [0, -1, 1]])
 
 print("Shape of Original Image", lightFieldImage.shape)
@@ -33,23 +34,27 @@ highPassFilter = highPassFilter[:, :, None]  # Add singleton dimension
 print("Shape of High Pass Filter", highPassFilter.shape)
 
 highPassFilteredImage = ndimage.convolve(lightFieldImage, highPassFilter)
-highPassFilteredImage = highPassFilteredImage.squeeze() # Remove singleton dimension
+highPassFilteredImage = highPassFilteredImage.squeeze()  # Remove singleton dimension
 
 cv.imshow("Original Image", lightFieldImage)
 cv.imshow("High Pass Filtered Image", highPassFilteredImage)
 
-binaryDepthMapImageWithClosingOpeningAndErosionHeight,binaryDepthMapImageWithClosingOpeningAndErosionWidth = binaryDepthMapImageWithClosingOpeningAndErosion.shape[:2]
+binaryDepthMapImageWithClosingOpeningAndErosionHeight, binaryDepthMapImageWithClosingOpeningAndErosionWidth = \
+    binaryDepthMapImageWithClosingOpeningAndErosion.shape[:2]
 
-resizedhighPassFilteredImage=cv.resize(highPassFilteredImage, (binaryDepthMapImageWithClosingOpeningAndErosionWidth,binaryDepthMapImageWithClosingOpeningAndErosionHeight), interpolation = cv.INTER_AREA)
-cv.imshow("High Pass Filtered Image after Resize", resizedhighPassFilteredImage)
+resizedHighPassFilteredImage = cv.resize(highPassFilteredImage, (
+    binaryDepthMapImageWithClosingOpeningAndErosionWidth, binaryDepthMapImageWithClosingOpeningAndErosionHeight),
+                                         interpolation=cv.INTER_AREA)
+cv.imshow("High Pass Filtered Image after Resize", resizedHighPassFilteredImage)
 
-maskedHighPassFilteredImage = cv.bitwise_and(resizedhighPassFilteredImage, resizedhighPassFilteredImage, mask=binaryDepthMapImageWithClosingOpeningAndErosion)
+maskedHighPassFilteredImage = cv.bitwise_and(resizedHighPassFilteredImage, resizedHighPassFilteredImage,
+                                             mask=binaryDepthMapImageWithClosingOpeningAndErosion)
 
 cv.imshow("Resized High Pass Filtered Image after Masking", maskedHighPassFilteredImage)
 
 # Using cv2.imwrite() method
 # Saving the image
-cv.imwrite('resizedhighPassFilteredImage_01.png', resizedhighPassFilteredImage)
+cv.imwrite('resizedHighPassFilteredImage_01.png', resizedHighPassFilteredImage)
 cv.imwrite('binaryDepthMapImageWithClosingOpeningAndErosion_01.png', binaryDepthMapImageWithClosingOpeningAndErosion)
 cv.imwrite('maskedHighPassFilteredImage_01.png', maskedHighPassFilteredImage)
 
