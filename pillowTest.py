7b7b673ef8c1bf8887c59cf334ac41881c0cc0f6
Banee Ishaque K <baneeishaque2@gmail.com>
2021-04-15 11:56:00 +0000

    clean

--- pillowTest.py
+++ pillowTest.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageOps
 
-depthImage = Image.open('depth_01.png');
+depthImage = Image.open('depth_01.png')
 depthImage.show()
 
 depthImageGray = ImageOps.grayscale(depthImage)
